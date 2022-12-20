67bb6d1e9b1f34cad3b60c05b134288cae949833
Neethu.K <neethurameesh2@gmail.com>
2022-12-20 11:51:27 +0530

    Initial commit

---
+++ src/Dummy-data.js
@@ -0,0 +1,29 @@
+export const Caro=[
+    {
+        id:1,
+        name:"Nature",
+        pic:"assets/nature1.jpeg",
+        place:"America",
+        quote:"Look deep into nature"
+        
+    
+    },
+    {
+        id:2,
+        name:"Beauty",
+        pic:"assets/nature2.jpeg",
+        place:"Africa",
+        quote:"study nature,love nature",
+        
+    },
+    {
+        id:3,
+        name:"Color",
+        pic:"assets/nature3.jpeg",
+        place:"Euorop",
+        quote:"Colors are the smile of nature"
+    
+    }
+
+
+]
